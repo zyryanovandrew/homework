251f9504d23e8704100d14826171121526b2b39a
zyryanovandrew <zyryanov.andrew@gmail.com>
2017-06-23 14:42:25 +0300

    Add files via upload

--- zyryanov_exam.py
+++ zyryanov_exam.py
@@ -1,16 +1,58 @@
 import os
 import re
 
-def count_sent():
-    sent_num = {}
+def preprocessing():
+    all_meta = []
     w = open('results.txt', 'w', encoding = 'utf-8')
     for el in os.listdir('news'):
         with open(os.path.join('news',el), 'r', encoding = 'Windows-1251') as f:
             article = f.read()
+            ## подсчет числа предложений
             sentences = re.findall(r'<se>', article)
-            sent_num[el] = len(sentences)
             template = '{}   {}\n'
             w.write(template.format(el, len(sentences)))
+
+            ## получение нужных метаданных
+            author = re.findall(r'<meta content=".+" name="author"></meta>', article)
+            authorstr = author[0]
+            authorstr = re.sub('<meta content="', '', authorstr)
+            authorstr = re.sub('" name="author"></meta>', '', authorstr)
+            topic = re.findall(r'<meta content=".+" name="topic"></meta>', article)
+            topicstr = topic[0]
+            topicstr = re.sub('<meta content="', '', topicstr)
+            topicstr = re.sub('" name="topic"></meta>', '', topicstr)
+            meta = el+'.xhtml'+','+authorstr+','+topicstr+'\n'
+            all_meta.append(meta)
+
+            ## создание списка слов, а затем биграм
+            words = []
+            wordsraw = re.findall('<w><ana lex=".+" gr=".+"></ana>.+</w>', article)
+            for el in wordsraw:
+                wordsrawstr = el
+                wordsrawstr = re.sub('<w><ana lex=".+" gr=".+"></ana>', '', wordsrawstr)
+                wordsrawstr = re.sub('</w>', '', wordsrawstr)
+                wordsrawstr = re.sub('`', '', wordsrawstr)
+                wordsrawstr = wordsrawstr.lower()
+                words.append(wordsrawstr)
+            bigrams = []
+            for ind in range(1, len(words) - 1):
+                bigrams.append(' '.join([words[ind - 1], words[ind]]))
+            
+            
     w.close()
-count_sent()
+    return bigrams, all_meta
+
+def data(all_meta):
+    w = open('metadata.csv', 'w', encoding = 'utf-8')
+    w.write('Название файла,Автор,Тематика текста\n')
+    for el in all_meta:
+        w.write(el)
+        
+
+
+
+bigrams, all_meta = preprocessing()
+print(bigrams)
+data(all_meta)
+
         
